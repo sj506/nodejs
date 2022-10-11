b8692176690ff107dca41be7617ddc1345dee76f
seung-jae <tmdwo5040@gmail.com>
2022-10-11 17:20:46 +0900

    회원가입

--- app/src/routes/home/index.js
+++ app/src/routes/home/index.js
@@ -9,6 +9,7 @@ const ctrl = require('./home.ctrl');
 // 라우팅
 router.get('/', ctrl.output.home);
 router.get('/login', ctrl.output.login);
+router.get('/register', ctrl.output.register);
 router.post('/login', ctrl.process.login);
 
 module.exports = router;
